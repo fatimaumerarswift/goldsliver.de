e96f2e756a9ab2927062bd215065b734a8a73007
fatimaumerarswift <fatimaumer@arswift.com>
2026-06-22 14:45:50 +0500

    del folder structure

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://fatimaumer862_db_user:fatima123@cluster0.fql9y1t.mongodb.net/goldensliver')
-    .then(()=>{
-        console.log("DB Connected")
-    })
-}
